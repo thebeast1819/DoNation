a033e6e218302b0d4ddfea09d95270c26a22a737
thebeast1819 <amanvaishya37@gmail.com>
2022-04-12 10:25:11 +0530

    Homepage modified

--- donation/src/Components/homepage/header/Header.js
+++ donation/src/Components/homepage/header/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import homeimg from '../../../assests/home3.png';
+import homeimg from '../../../assests/mainimg.jpg';
 
 
 import './header.css';
@@ -10,9 +10,9 @@ const Header = () => (
       <img src={homeimg} />
     </div>
     <div className="header-content3">
-      <h1>Hey, I am your College Mate!</h1>
-      <p>In such a big college, it's  difficult to find your pen drive if you forget it somewhere. Or, if you want to buy some old laptop because of lack of money, especially for freshers. Isn't it?</p>
-      <p>Don't worry, you are at right place where you can report if you have lost something or if you want to buy/sell something.</p>
+      <h1>dsfgbsdgfsdfdfgh</h1>
+      <p>fjhfjfyjjjjjjj</p>
+      <p>dsgshgfjgjg</p>
       <div className="header-content_input3">
       <a href="#feature"><p>Know More</p></a>
       </div>
