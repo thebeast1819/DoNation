dd11db5bc5ed73512931950588d9cfd673a4efcc
thebeast1819 <amanvaishya37@gmail.com>
2022-04-19 11:03:21 +0530

    Components added

--- donation/src/Components/homepage/header/Header.js
+++ donation/src/Components/homepage/header/Header.js
@@ -1,5 +1,8 @@
 import React from 'react';
-import homeimg from '../../../assests/mainimg.jpg';
+import image from '../../../assests/main_image.png';
+import homeimage from '../../../assests/homeimage.png';
+import homeimg from '../../../assests/home3.png';
+import home4 from '../../../assests/home4.png';
 
 
 import './header.css';
@@ -10,9 +13,9 @@ const Header = () => (
       <img src={homeimg} />
     </div>
     <div className="header-content3">
-      <h1>dsfgbsdgfsdfdfgh</h1>
-      <p>fjhfjfyjjjjjjj</p>
-      <p>dsgshgfjgjg</p>
+      <h1>Hey, I am your College Mate!</h1>
+      <p>In such a big college, it's  difficult to find your pen drive if you forget it somewhere. Or, if you want to buy some old laptop because of lack of money, especially for freshers. Isn't it?</p>
+      <p>Don't worry, you are at right place where you can report if you have lost something or if you want to buy/sell something.</p>
       <div className="header-content_input3">
       <a href="#feature"><p>Know More</p></a>
       </div>
--- donation/src/Components/homepage/nav_bar/Nav_bar.jsx
+++ donation/src/Components/homepage/nav_bar/Nav_bar.jsx
@@ -11,7 +11,8 @@ const Nav_bar = () => {
     <div className="navbar3">
       <div className="navbar-links3">
         <div className="navbar-links_logo3">
-          <p id='abcd'>DoNation</p>
+          <img src="https://res.cloudinary.com/kartikeyvaish/image/upload/v1642841457/Kolegia/logo_snpqqs.png" alt="" />
+          <p id='abcd'>olegia</p>
         </div>
         <div className="navbar-links_container3">
           <p><a href="#feature">Features</a></p>
